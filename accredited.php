1b8e621ed730e0af6315b8b33482bf223b6412c1
sherwin1231 <sherwin.malapitan09@gmail.com>
2025-02-08 21:15:04 +0800

    accredited page

---
+++ accredited.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="images/logo.png" />
+    <title>Rent IT - About</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <link rel="stylesheet" href="css/header.css">
+</head>
+<body>
+<div class="navbar">
+    <img src="images/logo.png" alt="Rent It" class="logo">
+    <div class="nav-links">
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="contact.php">Contact us</a>
+        <a href="login_page.php" class="login-link">Log in</a>
+    </div>
+</div>
+<br><br><br>
+
+<div class="container text-center">
+    <h2 style="color: black; font-size: 30px;">ACCREDITED DORMITORY/BOARDING HOUSE</h2>
+   
+    <div class="row">
+        <div class="col-md-4 col-sm-6 mb-4">
+            <img src="images/accredited/2.jpeg" alt="Accredited Photo 2" class="img-fluid rounded shadow">
+            <p style="color: black; font-size: 20px;">CLOUD BOARDING HOUSE </p>
+        </div>
+        <div class="col-md-4 col-sm-6 mb-4">
+            <img src="images/accredited/3.jpeg" alt="Accredited Photo 3" class="img-fluid rounded shadow">
+            <p style="color: black; font-size: 20px;">DATINGUINOO DORMITORY</p>
+        </div>
+        <div class="col-md-4 col-sm-6 mb-4">
+            <img src="images/accredited/4.jpeg" alt="Accredited Photo 4" class="img-fluid rounded shadow">
+            <p style="color: black; font-size: 20px;">ENGRACIA'S BOARDING HOUSE</p>
+        </div>
+        <div class="col-md-4 col-sm-6 mb-4">
+            <img src="images/accredited/5.jpeg" alt="Accredited Photo 5" class="img-fluid rounded shadow">
+            <p style="color: black; font-size: 20px;">QUEEN ELIZABETH LADIES HOME</p>
+        </div>
+        <div class="col-md-4 col-sm-6 mb-4">
+            <img src="images/accredited/6.jpeg" alt="Accredited Photo 6" class="img-fluid rounded shadow">
+            <p style="color: black; font-size: 20px;">K&M SPACE RENTAL</p>
+        </div>
+        <div class="col-md-4 col-sm-6 mb-4">
+            <img src="images/accredited/7.jpeg" alt="Accredited Photo 7" class="img-fluid rounded shadow">
+            <p style="color: black; font-size: 20px;">KRT BOARDING HOUSE</p>
+        </div>
+        <div class="col-md-4 col-sm-6 mb-4">
+            <img src="images/accredited/8.jpeg" alt="Accredited Photo 8" class="img-fluid rounded shadow">
+            <p style="color: black; font-size: 20px;">ROSALINDA ABUY BOARDING HOUSE</p>
+        </div>
+        <div class="col-md-4 col-sm-6 mb-4">
+            <img src="images/accredited/9.jpeg" alt="Accredited Photo 9" class="img-fluid rounded shadow">
+            <p style="color: black; font-size: 20px;">BREZA'S BOARDING HOUSE</p>
+        </div>
+        <div class="col-md-4 col-sm-6 mb-4">
+            <img src="images/accredited/10.jpeg" alt="Accredited Photo 10" class="img-fluid rounded shadow">
+            <p style="color: black; font-size: 20px;">SLSU DORMITORY</p>
+        </div>
+        <div class="col-md-4 col-sm-6 mb-4">
+            <img src="images/accredited/11.jpeg" alt="Accredited Photo 11" class="img-fluid rounded shadow">
+            <p style="color: black; font-size: 20px;">LORNA'S BOARDING HOUSE AND MINI STORE</p>
+        </div>
+        <div class="col-md-4 col-sm-6 mb-4">
+            <img src="images/accredited/12.jpeg" alt="Accredited Photo 12" class="img-fluid rounded shadow">
+            <p style="color: black; font-size: 20px;">BM AMPARO APARTMENT</p>
+        </div>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
