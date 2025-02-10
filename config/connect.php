3f22518e3946b2015d139d64b5b5f391b4fdefd0
Gregg Code <felicisimojv@gmail.com>
2025-02-10 23:05:56 +0800

    update connection

--- config/connect.php
+++ config/connect.php
@@ -1,9 +1,15 @@
 <?php 
 
+	// Uncomment if using local repo
+	// $servername = "localhost";
+	// $username = "root";
+	// $password = "";
+	// $dbname = "rent_it";
+
 	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "rent_it";
+	$username = "u480853257_rentit_new";
+	$password = "12kls.Smalltank";
+	$dbname = "u480853257_rentit_new";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,5 +18,7 @@
 	if ($conn->connect_error) {
 	   die("Connection failed: " . $conn->connect_error);
 	} 
+
+	$conn->set_charset("utf8mb4");
 
 ?>
