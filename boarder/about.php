0d33fecd3d50090fb100dc3e8253ae55561c91fc
Gregg Code <felicisimojv@gmail.com>
2025-02-10 19:20:44 +0800

    Fix responsiveness of navigation boarder

--- boarder/about.php
+++ boarder/about.php
@@ -100,6 +100,9 @@ if(!isset($_SESSION["AccountID"])){
         <div class="bar"></div>
     </div>
 </div>
+<?php
+    include("../components/ResponsiveNavBoarder.html");
+?>
     <div class="dashboard-container">
         <div class="dashboard-box">
             <div class="dashboard">
@@ -245,7 +248,7 @@ if(!isset($_SESSION["AccountID"])){
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <script>
         function toggleMenu() {
-            const navLinks = document.querySelector('.nav-links');
+            const navLinks = document.querySelector('.navbarsmall');
             navLinks.style.display = (navLinks.style.display === 'flex') ? 'none' : 'flex';
         }
     </script>
--- boarder/landing_page.php
+++ boarder/landing_page.php
@@ -220,6 +220,9 @@ if(!isset($_SESSION["AccountID"])){
         <div class="bar"></div>
     </div>
 </div>
+<?php
+    include("../components/ResponsiveNavBoarder.html");
+?>  
     <div class="dashboard-container">
         <div class="dashboard-box">
             <div class="dashboard">
@@ -399,7 +402,7 @@ if(!isset($_SESSION["AccountID"])){
         
     <script>
         function toggleMenu() {
-            const navLinks = document.querySelector('.nav-links');
+            const navLinks = document.querySelector('.navbarsmall');
             navLinks.style.display = (navLinks.style.display === 'flex') ? 'none' : 'flex';
         }
         let map;
