8914b684da522864b2b1c2e7f9c2c3362228534b
Gregg Code <felicisimojv@gmail.com>
2025-02-12 22:07:19 +0800

    Db connection

--- config/connect.php
+++ config/connect.php
@@ -1,15 +1,16 @@
 <?php 
 
 	// Uncomment if using local repo
-	// $servername = "localhost";
-	// $username = "root";
-	// $password = "";
-	// $dbname = "rent_it";
-
 	$servername = "localhost";
-	$username = "u480853257_rentit_new";
-	$password = "12kls.Smalltank";
-	$dbname = "u480853257_rentit_new";
+	$username = "root";
+	$password = "";
+	$dbname = "rent_it";
+
+	// Uncomment if using remote repo
+	// $servername = "localhost";
+	// $username = "u480853257_rentit_new";
+	// $password = "12kls.Smalltank";
+	// $dbname = "u480853257_rentit_new";
 
 	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
